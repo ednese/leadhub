64336c5fee9f7c299fe65e8af541ad58ce6d1524
ednese <evancours94@gmail.com>
2024-03-08 17:17:32 +0100

    first commit

---
+++ src/components/Author.jsx
@@ -0,0 +1,66 @@
+import Image from 'next/future/image'
+import Link from 'next/link'
+
+import { GridPattern } from '@/components/GridPattern'
+import { SectionHeading } from '@/components/SectionHeading'
+import authorImage from '@/images/avatars/author.jpg'
+
+function TwitterIcon(props) {
+  return (
+    <svg aria-hidden="true" viewBox="0 0 40 40" {...props}>
+      <path d="M13.817 33.753c12.579 0 19.459-10.422 19.459-19.458 0-.297 0-.592-.02-.884a13.913 13.913 0 0 0 3.411-3.543 13.65 13.65 0 0 1-3.928 1.077 6.864 6.864 0 0 0 3.007-3.784 13.707 13.707 0 0 1-4.342 1.66 6.845 6.845 0 0 0-11.655 6.239A19.417 19.417 0 0 1 5.654 7.915a6.843 6.843 0 0 0 2.117 9.128 6.786 6.786 0 0 1-3.104-.853v.086a6.842 6.842 0 0 0 5.487 6.704 6.825 6.825 0 0 1-3.088.116 6.847 6.847 0 0 0 6.39 4.75A13.721 13.721 0 0 1 3.334 30.68a19.36 19.36 0 0 0 10.483 3.066" />
+    </svg>
+  )
+}
+
+export function Author() {
+  return (
+    <section
+      id="author"
+      aria-labelledby="author-title"
+      className="relative scroll-mt-14 pb-3 pt-8 sm:scroll-mt-32 sm:pb-16 sm:pt-10 lg:pt-16"
+    >
+      <div className="absolute inset-x-0 bottom-0 top-1/2 text-slate-900/10 [mask-image:linear-gradient(transparent,white)]">
+        <GridPattern x="50%" y="100%" />
+      </div>
+      <div className="relative mx-auto max-w-5xl pt-16 sm:px-6">
+        <div className="bg-slate-50 pt-px sm:rounded-6xl">
+          <div className="relative mx-auto -mt-16 h-44 w-44 overflow-hidden rounded-full bg-slate-200 md:float-right md:h-64 md:w-64 md:[shape-outside:circle(40%)] lg:mr-20 lg:h-72 lg:w-72">
+            <Image
+              className="absolute inset-0 h-full w-full object-cover"
+              src={authorImage}
+              alt=""
+              sizes="(min-width: 1024px) 18rem, (min-width: 768px) 16rem, 11rem"
+            />
+          </div>
+          <div className="px-4 py-10 sm:px-10 sm:py-16 md:py-20 lg:px-20 lg:py-32">
+            <SectionHeading number="5" id="author-title">
+              CEO
+            </SectionHeading>
+            <p className="mt-8 font-display text-5xl font-extrabold tracking-tight text-slate-900 sm:text-6xl">
+              <span className="block text-blue-600">Evan Sende</span></p>
+            <p className="mt-4 text-lg tracking-tight text-slate-700">
+              Fort de plus de 5 ans d'expérience en développement web,
+              j'ai évolué au sein d'entreprises confrontées à des défis majeurs tels que le SEO et l'optimisation de la conversion,
+              notamment sur des plateformes telles que le site e-commerce Back Market.
+              Ma curiosité m'a poussé à me plonger activement dans les enjeux immobiliers pendant une année de crise,
+              et mes échanges approfondis avec des agents immobiliers m'ont révélé un besoin criant non satisfait pendant cette période tumultueuse.
+              C'est ainsi que j'ai fondé une agence spécialisée dans la création de sites web immobiliers sur mesure,
+              mettant l'accent sur l'optimisation de la conversion. Mon expertise vise à offrir des solutions adaptées,
+              reflétant l'identité de chaque agence et répondant aux exigences spécifiques du secteur immobilier.
+            </p>
+            <p className="mt-8">
+              <Link
+                href="#"
+                className="inline-flex items-center text-base font-medium tracking-tight text-blue-600"
+              >
+                <TwitterIcon className="h-10 w-10 fill-current" />
+                <span className="ml-4">Suivez moi sur Twitter</span>
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
---
+++ src/components/Resources.jsx
@@ -0,0 +1,107 @@
+import Image from 'next/future/image'
+
+import { Container } from '@/components/Container'
+import { SectionHeading } from '@/components/SectionHeading'
+import abstractBackgroundImage from '@/images/resources/abstract-background.png'
+import discordImage from '@/images/resources/seo.svg'
+import figmaImage from '@/images/resources/ad.svg'
+import videoPlayerImage from '@/images/resources/video-player.svg'
+
+const resources = [
+  {
+    title: 'Site web prenium',
+    description:
+      "Nous concevons des sites web avec pour objectif principal d'optimiser la conversion en transformant les visiteurs en prospects qualifiés.",
+    image: function VideoPlayerImage() {
+      return (
+        <div className="absolute inset-0 flex items-center justify-center">
+          <Image
+            className="absolute inset-0 h-full w-full object-cover"
+            src={abstractBackgroundImage}
+            alt=""
+            sizes="(min-width: 1280px) 21rem, (min-width: 1024px) 33vw, (min-width: 768px) 19rem, (min-width: 640px) 50vw, 100vw"
+          />
+          <Image
+            className="relative"
+            src={videoPlayerImage}
+            alt=""
+            unoptimized
+          />
+        </div>
+      )
+    },
+  },
+  {
+    title: 'Référencement SEO',
+    description:
+      "Nous optimisons les sites web pour les moteurs de recherche afin d'améliorer leur visibilité en ligne et d'attirer plus de trafic qualifié.",
+    image: function DiscordImage() {
+      return (
+        <div className="absolute inset-0 flex items-center justify-center bg-[#6366F1]">
+          <Image src={discordImage} className="h-full w-1/2" alt="" unoptimized />
+        </div>
+      )
+    },
+  },
+  {
+    title: 'Campagnes publicitaires',
+    description:
+      'Nous concevons et exécutons des campagnes publicitaires, ciblant stratégiquement dans votre zone géographique des leads qualifiés.',
+    image: function FigmaImage() {
+      return (
+        <div className="absolute inset-0 flex items-center justify-center bg-green-600">
+          <Image src={figmaImage} className="h-full w-1/2" alt="" unoptimized />
+        </div>
+      )
+    },
+  },
+]
+
+export function Resources() {
+  return (
+    <section
+      id="resources"
+      aria-labelledby="resources-title"
+      className="scroll-mt-14 py-16 sm:scroll-mt-32 sm:py-20 lg:py-32"
+    >
+      <Container>
+        <SectionHeading number="3" id="resources-title">
+          Solutions
+        </SectionHeading>
+        <p className="mt-8 font-display text-4xl font-bold tracking-tight text-slate-900">
+          Conception et intégration de sites web personnalisés
+        </p>
+        <p className="mt-4 text-lg tracking-tight text-slate-700">
+          Nous offrons des services de conception et d'intégration de sites web sur mesure pour les agences immobilières.
+          Notre équipe d'experts travaille en étroite collaboration avec nos clients pour créer des sites web attrayants
+          et fonctionnels qui répondent à leurs besoins spécifiques.
+        </p>
+      </Container>
+      <Container size="lg" className="mt-16">
+        <ol
+          role="list"
+          className="-mx-3 grid grid-cols-1 gap-y-10 lg:grid-cols-3 lg:text-center xl:-mx-12 xl:divide-x xl:divide-slate-400/20"
+        >
+          {resources.map((resource) => (
+            <li
+              key={resource.title}
+              className="grid auto-rows-min grid-cols-1 items-center gap-8 px-3 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-1 xl:px-12"
+            >
+              <div className="relative h-48 overflow-hidden rounded-2xl shadow-lg sm:h-60 lg:h-40">
+                <resource.image />
+              </div>
+              <div>
+                <h3 className="text-base font-medium tracking-tight text-slate-900">
+                  {resource.title}
+                </h3>
+                <p className="mt-2 text-sm text-slate-600">
+                  {resource.description}
+                </p>
+              </div>
+            </li>
+          ))}
+        </ol>
+      </Container>
+    </section>
+  )
+}
